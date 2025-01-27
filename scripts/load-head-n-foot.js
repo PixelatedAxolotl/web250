c5e1888d1cb07ab25c9fcfb774eb4ede5ced08e2
PixelatedAxolotl <dontpokethechimera@gmail.com>
2025-01-26 20:23:22 -0500

    Testing with just the template first to see how it goes

---
+++ scripts/load-head-n-foot.js
@@ -0,0 +1,54 @@
+"use strict";
+
+async function loadHTML(elementName, filePath)
+{
+    const element = document.querySelector(elementName);
+
+    try 
+    {
+        const response = await fetch(filePath);
+      
+        // Check if the response is successful
+        if (!response.ok) 
+        {
+            console.error('Failed to fetch ' + filePath + ': Status ' + response.status);
+            return;
+        }
+    
+        // Grab html from the response
+        const content = await response.text();
+        
+        // Add html to element
+        if (element) 
+        {
+            element.innerHTML = content;
+        } 
+        else 
+        {
+            console.error('Element not found: ' + elementName);
+        }
+    } catch (error) 
+    {
+      // Handle any fetch-related errors
+      console.error('Error loading ' + filePath + ':', error);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    //Don't display anything until header and footer have been loaded
+    document.querySelector('body').style.display = "none";
+    
+    //Add header and footer content for the page
+    Promise.all
+    ([
+        loadHTML('header', 'fragments/header.html'),
+        loadHTML('footer', 'fragments/footer.html')
+    ])
+    .then(() => 
+    {
+        //Set display back to block after header and footer are loaded
+        document.querySelector('body').style.display = "block";
+    });
+});
