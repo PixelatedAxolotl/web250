1d32938357da3ad169be5ff258965cf92166be43
PixelatedAxolotl <dontpokethechimera@gmail.com>
2025-05-02 13:37:14 -0400

    fixed validation

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -3,72 +3,68 @@ document.addEventListener("DOMContentLoaded", function(event)
     document.querySelector(`button[type="submit"]`).addEventListener('click', function(event)
     {
 
-        if (! document.querySelector(`button[type="submit"]`).checkValidity())
+        console.log(document.querySelector(`form`).checkValidity());
+        console.log(document.querySelector(`button[type="submit"]`).checkValidity());
+        if (document.querySelector(`form`).checkValidity())
         {
-            return;
+            event.preventDefault(); // Prevent form submission
+
+            // Name fields
+            var firstName = document.getElementById('first-name').value;
+            const middleInitial = document.getElementById('middle-initial').value;
+            const lastName = document.getElementById('last-name').value;
+
+            // Assemble Welcome message
+            // Correct capitalization if needed
+            firstName = firstName.replace(/^./, (char) => (char.toUpperCase()));
+            // (sort of) Accounts for multiword last names where the first word should not be capitalized
+            if (! /[A-Z]/.test(lastName)) lastName.replace(/^./, (char) => char.toUpperCase());
+            if (middleInitial) middleInitial = middleInitial.replace(/^./, (char) => char.toUpperCase());
+            
+            var welcomeMessage = "Welcome, " + firstName;
+
+            if (middleInitial) welcomeMessage += " " + middleInitial + ".";
+            if (lastName) welcomeMessage += " " + lastName;
+
+            const messageHeader = document.querySelector('main section:last-child h3');
+            welcomeMessage += "! Here are your results.";
+            messageHeader.innerHTML = welcomeMessage;
+
+            // Fizz Buzz word/number pairs
+            const word1 = document.getElementById('word1').value;
+            const divisor1 = parseInt(document.getElementById('divisor1').value, 10);
+
+            const word2 = document.getElementById('word2').value;
+            const divisor2 = parseInt(document.getElementById('divisor2').value, 10);
+
+            const word3 = document.getElementById('word3').value;
+            const divisor3 = parseInt(document.getElementById('divisor3').value, 10);
+
+            // Default word and count limit
+            const defaultWord = document.getElementById('main-word').value;
+            const countLimit = parseInt(document.getElementById('count-limit').value, 10);
+            
+            // put results in an unordered list
+            const list = document.querySelector('section ul');
+            list.innerHTML = ''; // Clear previous results
+            
+            // output word if number can be divided by the word's given divisor
+            for (let i = 1; i <= countLimit; i++) 
+            {
+                let output = '';
+        
+                if (divisor1 && i % divisor1 === 0) output += word1 + " ";
+                if (divisor2 && i % divisor2 === 0) output += word2 + " ";
+                if (divisor3 && i % divisor3 === 0) output += word3 + " ";
+        
+                if (!output) output = defaultWord;
+        
+                // add to list
+                const listItem = document.createElement('li');
+                listItem.textContent = i + ": " + output;
+                list.appendChild(listItem);
+            } // End of for loop
         }
-
-        event.preventDefault(); // Prevent form submission
-
-        // Name fields
-        var firstName = document.getElementById('first-name').value;
-        const middleInitial = document.getElementById('middle-initial').value;
-        const lastName = document.getElementById('last-name').value;
-
-        // Assemble Welcome message
-        // Correct capitalization if needed
-        firstName = firstName.replace(/^./, (char) => (char.toUpperCase()));
-        // (sort of) Accounts for multiword last names where the first word should not be capitalized
-        if (! /[A-Z]/.test(lastName)) lastName.replace(/^./, (char) => char.toUpperCase());
-        if (middleInitial) middleInitial = middleInitial.replace(/^./, (char) => char.toUpperCase());
-        
-        var welcomeMessage = "Welcome, " + firstName;
-
-        if (middleInitial) welcomeMessage += " " + middleInitial + ".";
-        if (lastName) welcomeMessage += " " + lastName;
-
-        welcomeMessage += "! Here are your results.";
-
-        const messageHeader = document.createElement('h3');
-        messageHeader.innerHTML = welcomeMessage;
-        document.querySelector('main section:last-child').prepend(messageHeader);
-
-
-        // Fizz Buzz word/number pairs
-        const word1 = document.getElementById('word1').value;
-        const divisor1 = parseInt(document.getElementById('divisor1').value, 10);
-
-        const word2 = document.getElementById('word2').value;
-        const divisor2 = parseInt(document.getElementById('divisor2').value, 10);
-
-        const word3 = document.getElementById('word3').value;
-        const divisor3 = parseInt(document.getElementById('divisor3').value, 10);
-
-        // Default word and count limit
-        const defaultWord = document.getElementById('main-word').value;
-        const countLimit = parseInt(document.getElementById('count-limit').value, 10);
-        
-        // put results in an unordered list
-        const list = document.querySelector('section ul');
-        list.innerHTML = ''; // Clear previous results
-        
-        // output word if number can be divided by the word's given divisor
-        for (let i = 1; i <= countLimit; i++) 
-        {
-            let output = '';
-    
-            if (divisor1 && i % divisor1 === 0) output += word1 + " ";
-            if (divisor2 && i % divisor2 === 0) output += word2 + " ";
-            if (divisor3 && i % divisor3 === 0) output += word3 + " ";
-    
-            if (!output) output = defaultWord;
-    
-            // add to list
-            const listItem = document.createElement('li');
-            listItem.textContent = i + ": " + output;
-            console.log(listItem);
-            list.appendChild(listItem);
-        } // End of for loop
 
     }); // 
 
